08b5aa87e173b9d673e36809c6c0402539b1751f
SahidProgrammer <2109133@upy.edu.mx>
2022-02-09 08:33:00 -0600

    Merge branch 'master' of https://github.com/SahidStudent/Algorithms-Fundamentals

---
+++ unit1/Tarea Algoritmo para determinar si un número es PAR o IMPAR.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Ingresa un numero\n");
+    scanf("%i",&n);
+    if(n % 2==0){
+        printf("El número es par");
+}
+
+ else{
+     printf("El número es impar");
+}
+
+return 0;
+}
